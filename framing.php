096253f7f4e5cd423e4190b4b0008339e282686b
Zain Faheem SE2019 <zain.faheem.2019@uni.strath.ac.uk>
2021-10-08 18:13:46 +0100

    Layer B complete

--- framing.php
+++ framing.php
@@ -9,29 +9,51 @@
 <div>
     <h1>Frame Price Estimator</h1>
     <?php
-    $width = isset($_POST["width"]) ? $_POST["width"] : 0;
-    $height = isset($_POST["height"]) ? $_POST["height"] : 0;
+    $width = $_POST["width"];
+    $height = $_POST["height"];
+    $wUnit  = $_POST["units"];
+    $hUnit = $_POST["units"];
 
 
-    if(empty($width) || empty($height)) {//conditions for erroneous submission
-        //Need to output the form
-        if($_SERVER["REQUEST_METHOD"]==="POST"){
-            echo "<p>Please provide suitable frame dimensions.</p>"; //Error message
-        }
-        ?>
-        <form action="framing.php" method="post">
-            <p>Please enter your photo sizes to get a framing cost estimate.</p>
-            <p>Photo Width: <input type="number" name="width" min = "1" /> </p>
-            <p>Photo Height: <input type="number" name="height" min = "1"/> </p>
-            <p><input type="submit"/> </p>
-        </form>
-        <?php
-    } else {
-        $area = ($width/1000) * ($height/1000);      // divide by 1000 to convert to metres
-        $price = ($area * $area) + (100*$area) + 6;
-        $priceTo2dec = round($price,2);
-        echo "<p>Your frame will cost £$priceTo2dec</p>";
+
+    if($width =="" || $width == 0) {
+        echo "<p>Width is not valid: Please enter the width.</p>";
     }
+
+    if($height=="" || $height==0){
+            echo "<p>Height is not valid: Please enter the height.</p>";
+        }
+
+    switch($wUnit){
+        case "mm":   $width /= 1000; break;
+        case "cm":    $width /= 100;  break;
+        case "inches": $width /= 39.37; break;
+
+    }
+
+    switch($hUnit){
+        case "mm":   $height /= 1000; break;
+        case "cm":    $height /= 100;  break;
+        case "inches": $height /= 39.37; break;
+
+    }
+    $area = $width * $height;
+    $price = round(($area * $area) + (100*$area) + 6,2);
+
+    $postage = $_POST["postage"];
+    $longestEdge = max($width,$height);
+    $postageCost= 0;
+
+    if($postage == "Economy"){
+     $postageCost = round((2*$longestEdge) + 4,2);
+    } elseif ($postage == "Rapid") {
+        $postageCost = round((3*$longestEdge) + 8,2);
+    } elseif ($postage == "Next Day") {
+        $postageCost = round((5*$longestEdge) + 12,2);
+    }
+    $totalCost = $price + $postageCost;
+    $totalCostWithVAT = round(($totalCost * 0.2) + $totalCost,2);
+    echo "<p>Your frame will cost £$price plus $postage postage of £$postageCost giving a total price of £$totalCostWithVAT including VAT.</p>";
     ?>
 
 </div>
